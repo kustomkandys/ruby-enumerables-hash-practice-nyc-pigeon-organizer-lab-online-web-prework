12093377dc6e3d4fc43c66e5eb10991e836fcf12
Keri Deemer <kerid18@att.net>
2019-09-08 19:10:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon = {}
+  data.each do | key, hash|
+    hash.each do |attribute, name_array|
+      name_array.each do |name|
+        if new_pigeon[name].nil?
+          new_pigeon[name] = {}
+          new_pigeon[name][:color] = {}
+        end
+        if key == :color 
+          new_pigeon[name][key] << attribute.to_s 
+        else
+          new_pigeon[name][key] = {}
+          new_pigeon[name][key] << attribute.to_s
+        end
+    end
+  end
 end
